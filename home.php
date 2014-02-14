95610efb01433d220077bdf0062eb509057c6c26
ctmdev <ctmdev33@gmail.com>
2014-02-14 17:45:54 -0500

    change name
    I do not approve of your put downs

--- home.php
+++ home.php
@@ -11,7 +11,7 @@
   <body>
     <h1>Testing 123</h1>
 
-    <h2>Colin sucks</h2>
+    <h2>John sucks</h2>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
