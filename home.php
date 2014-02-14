2c90ceae3db87261f392d0eb153e1e14ce333bd7
jljohnlu <jljohnlu@gmail.com>
2014-02-14 18:25:46 -0500

    Additional Text
    To elaborate on my earlier insult.

--- home.php
+++ home.php
@@ -12,6 +12,8 @@
     <h1>Testing 123</h1>
 
     <h2>John sucks</h2>
+    
+    <h3>Colin actually sucks </h3>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
