7c5b68a31d2f89586d4cf1e31d09ff807d6ecbd4
jljohnlu <jljohnlu@gmail.com>
2014-03-10 01:49:18 -0400

    new updates
    changed home.php and aboutus.php

---
+++ aboutus.php
@@ -0,0 +1,135 @@
+<html>
+	<head>
+  		<title>Grant Fellow</title>
+ 	</head>
+	
+	<script src="jquery.js"></script> 
+    <script> 
+    $(function(){
+      $("#footer").load("footertemplate.html"); 
+    });
+    </script> 
+
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+
+      	<div class="container-fluid">
+
+      		<div class="navbar-header">
+      		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			    </button>
+			    <a class="navbar-brand" href="home.php"><img src="images/logo_temporary.png"></a>		      	
+	   		</div>
+
+	   		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">  <!--navbar-nav is the uncollapsed bar-->
+					<li class="dropdown">
+		        		<li><a href="#">Log In</a></li>
+		        		<li><a href="#">Features</a></li>
+		        		<li><a href="#">About Us</a></li>
+		        		<li><a href="#">Contact Us</a></li>
+		        	</li>
+	    		</ul>
+	    	</div>
+	   	</div>
+	</nav>
+
+<div class="hero">
+   	<div class="container">
+      	<div class="col-md-12">
+        	<h2>World Class Team</h2>
+      	</div>
+    </div>
+    <div class="hero-tab-images">
+        <div class="hero-image active" style="background-image: url(http://mycoolwallpaper.com/wp-content/uploads/2013/12/high-definition-ocean-wallpapers-114.jpg);"></div>
+    </div>
+  </div>
+
+<div class="container-fluid">
+<div class="row">
+	<hr class="featurette-divider">	
+    <div class="col-md-2">
+		<ul class="list-group">
+	  		<li class="list-group-item"><a href="#team">Team</a></li>
+	  		<li class="list-group-item"><a href="#advisors">Advisors</a></li>
+		</ul>
+	</div>
+
+    <div class="col-md-10">
+		<div class="row">
+		<a name="team"></a> 
+		<h1 style="text-align: center">Team</h1>
+			<div class="col-md-2">
+			  	<img src="images/professor_temporary.png" alt="..." class="image-bio-thumbnail">
+			</div>
+			<div class="col-md-10">
+			  	<h2 class="featurette-small">John Lu</h2>
+			  	<h4><span class="text-muted">Mr. Awesome</span></h4>
+			  	<p class="lead-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</div>
+		</div>	
+		<div class="row">
+			<div class="col-md-2">
+			  	<img src="images/professor_temporary.png" alt="..." class="image-bio-thumbnail">
+			</div>
+			<div class="col-md-10">
+			  	<h2 class="featurette-small">John Lu</h2>
+			  	<h4><span class="text-muted">Mr. Awesome</span></h4>
+			  	<p class="lead-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+			  	<img src="images/professor_temporary.png" alt="..." class="image-bio-thumbnail">
+			</div>
+			<div class="col-md-10">
+			  	<h2 class="featurette-small">John Lu</h2>
+			  	<h4><span class="text-muted">Mr. Awesome</span></h4>
+			  	<p class="lead-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-2">
+			  	<img src="images/professor_temporary.png" alt="..." class="image-bio-thumbnail">
+			</div>
+			<div class="col-md-10">
+			  	<h2 class="featurette-small">John Lu</h2>
+			  	<h4><span class="text-muted">Mr. Awesome</span></h4>
+			  	<p class="lead-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</div>
+		</div>
+
+		<hr class="featurette-divider">	
+		<div class="row">
+		<a name="advisors"></a>	
+		<h1 style="text-align: center">Advisors</h1>
+			<div class="col-md-2">
+			  	<img src="images/professor_temporary.png" alt="..." class="image-bio-thumbnail">
+			</div>
+			<div class="col-md-10">
+			  	<h2 class="featurette-small">John Lu</h2>
+			  	<h4><span class="text-muted">Mr. Awesome</span></h4>
+			  	<p class="lead-small">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div id="footer">
+ 	<div class="container">
+    	<p style="float: right;"> Sign Up | Privacy Policy | Contact Us </p>
+    </div>
+</div>
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+    <script src="js/custom.js"></script>
+</html>
---
+++ home.php
@@ -0,0 +1,135 @@
+<html>
+	<head>
+  		<title>JDLC</title>
+ 	</head>
+	
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+
+      	<div class="container-fluid">
+
+      		<div class="navbar-header">
+      		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			    </button>
+			    <a class="navbar-brand" href="home.php"><img src="images/logo_temporary.png"></a>		      	
+	   		</div>
+
+	   		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">  <!--navbar-nav is the uncollapsed bar-->
+					<li class="dropdown">
+		        		<li><a href="#">Log In</a></li>
+		        		<li><a href="#">Features</a></li>
+		        		<li><a href="aboutus.php">About Us</a></li>
+		        		<li><a href="#">Contact Us</a></li>
+		        	</li>
+	    		</ul>
+	    	</div>
+	   	</div>
+
+	</nav>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <div id="logo">
+          <img src="images/banner_temporary1.jpg">
+          </div>
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>The Ultimate Button</h1>
+              <p>JDLC Manufactured</p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+        	<div id="logo">
+          <img src="images/banner_temporary2.jpg">
+          </div>
+          <div class="container">
+
+            <div class="carousel-caption">
+              <h1>Why us?</h1>
+              <p>We offer world class support in many areas</p>
+            </div>
+          </div>
+        </div>
+        <div class="item">
+         	<div id="logo">
+          <img src="images/banner_temporary3.jpg">
+           	</div>
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>How do I sign up?</h1>
+              <p><a class="btn btn-primary btn-lg" role="button">Click here</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+    </div>
+
+</div>
+
+<div class="container">
+
+	<div class="row">
+	    <div class="col-lg-4">
+	        <h2>Affordable</h2>
+	        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	        <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+	    	</br>
+	    </div>
+	    <div class="col-lg-4">
+	        <h2>Customizable</h2>
+	        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	        <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+	    	</br>
+	    </div>
+	    <div class="col-lg-4">
+	        <h2>Simple</h2>
+	        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+	        <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+	    	</br>	   
+	    </div>
+	</div>
+
+<hr class="featurette-divider">
+	<div class="row featurette">
+		<div class="col-md-7">
+		  	<h2 class="featurette-heading">Why are we awesome? <span class="text-muted">Because.</span></h2>
+		  	<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		</div>
+		<div class="col-md-5">
+		  	<img class="featurette-image img-responsive" src="images/professor_temporary.png">
+		</div>
+	</div>
+
+	<hr class="featurette-divider">
+
+</div>
+
+<div id="footer">
+    <div class="container">
+    	<p style="float: right;"> Sign Up | Privacy Policy | Contact Us </p>
+    </div>
+</div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+
+</html>
