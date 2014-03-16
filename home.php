640c9e129d94287daa7319eef4a2ea2f5596a9f4
jljohnlu <jljohnlu@gmail.com>
2014-03-16 14:16:47 -0400

    new website

--- home.php
+++ home.php
@@ -1,11 +1,11 @@
 <html>
 	<head>
-  		<title>JDLC</title>
+  		<title>Grant Fellow</title>
  	</head>
 	
 	<link href="css/bootstrap.css" rel="stylesheet">
 	<link href="css/custom.css" rel="stylesheet">
-	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<link href="css/jquery.dynatable.css" rel="stylesheet">
 
 	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
 
@@ -18,16 +18,31 @@
 			        <span class="icon-bar"></span>
 			        <span class="icon-bar"></span>
 			    </button>
-			    <a class="navbar-brand" href="home.php"><img src="images/logo_temporary.png"></a>		      	
+			    <a class="navbar-brand" href="home.php"><img src="images/logos/logo_horizontal.png" style="max-height: 50px;"></a>		      	
 	   		</div>
 
 	   		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav navbar-right">  <!--navbar-nav is the uncollapsed bar-->
-					<li class="dropdown">
-		        		<li><a href="#">Log In</a></li>
-		        		<li><a href="#">Features</a></li>
-		        		<li><a href="aboutus.php">About Us</a></li>
-		        		<li><a href="#">Contact Us</a></li>
+
+		        		<li><a href="home.php">Home</a></li>
+		        		<li class="dropdown">
+		        		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Team<b class="caret"></b></a>
+		        		<ul class="dropdown-menu">
+				            <li><a href="mission.php">Mission</a></li>
+				            <li><a href="aboutus.php">About Us</a></li>
+				            <li><a href="advisors.php">Advisors</a></li>
+				        </ul>
+				        </li>
+		        		<li><a href="design.php">Design</a></li>
+		        		<li><a href="make.php">Make</a></li>
+		        		<li class="dropdown">
+		        		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Community<b class="caret"></b></a>
+		        		<ul class="dropdown-menu">
+				            <li><a href="forum.php">Forum</a></li>
+				            <li><a href="contactus.php">Contact Us</a></li>
+				            <li><a href="faq.php">FAQ</a></li>
+				        </ul>
+				        </li>
 		        	</li>
 	    		</ul>
 	    	</div>
@@ -44,21 +59,20 @@
       <div class="carousel-inner">
         <div class="item active">
           <div id="logo">
-          <img src="images/banner_temporary1.jpg">
+          <img src="images/banner_temporary3.jpg" style="width: 100%; height: auto; max-height: 500px;">
           </div>
           <div class="container">
             <div class="carousel-caption">
-              <h1>The Ultimate Button</h1>
-              <p>JDLC Manufactured</p>
+              <h1>Grant Fellow</h1>
+              <p>The Common Application for Grants</p>
             </div>
           </div>
         </div>
         <div class="item">
         	<div id="logo">
-          <img src="images/banner_temporary2.jpg">
+          <img src="images/banner_temporary3.jpg" style="width: 100%; height: auto; max-height: 500px;">
           </div>
           <div class="container">
-
             <div class="carousel-caption">
               <h1>Why us?</h1>
               <p>We offer world class support in many areas</p>
@@ -67,12 +81,12 @@
         </div>
         <div class="item">
          	<div id="logo">
-          <img src="images/banner_temporary3.jpg">
+          <img src="images/banner_temporary3.jpg" style="width: 100%; height: auto; max-height: 500px;">
            	</div>
           <div class="container">
             <div class="carousel-caption">
               <h1>How do I sign up?</h1>
-              <p><a class="btn btn-primary btn-lg" role="button">Click here</a></p>
+              <p><a class="btn btn-primary btn-lg" role="button"href="login.php">Click here</a></p>
             </div>
           </div>
         </div>
@@ -80,50 +94,52 @@
       <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
       <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
     </div>
-
 </div>
 
 <div class="container">
+<hr class="featurette-divider" style="margin-top: -30px">
+
+  <div class="row featurette">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-8">
+        <h2 class="featurette-heading">So what do we do? <span class="text-muted">Many Things.</span></h2>
+        <p class="lead">Grant Fellow is an online aggregator, common application, and application manager for grants. We want to revolutionize the grant application process to make it easier for professors to find the grants that they want and for grant organizations to find the researchers that they need. Our grant aggregator collects information about both public and private grants and posts them onto our database. Instead of spending time looking through pages upon pages of irrelevant grants, professors can now use our proprietary matchmaking system, which ranks grants based on their relevance. Our common application system saves time because researchers will no longer need to input information that is already on their profile. Finally, our application manager helps researchers keep track of upcoming application deadlines, so that no opportunities are ever missed. All of this is now available through a single, intuitive system, Grant Fellow. </p>
+    </div>
+    <div class="col-md-3">
+        <img class="featurette-image img-responsive" src="images/images/female_professor_shadow.jpg" style="max-height: 400px;">
+    </div>
+  </div>
+
+  <hr class="featurette-divider">
 
 	<div class="row">
 	    <div class="col-lg-4">
-	        <h2>Affordable</h2>
-	        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-	        <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+          <h2 style="text-align:center">Reliable</h2>
+          <div style="text-align:center"><img src="images/icons/reliable.jpg" alt="..." class="image-bio-thumbnail-borderless-nomargin"></div>
+	        <p>Grant Fellow prides itself in our 24/7 customer support. We know how frustrating it can be dealing with buggy software, so we try our very best to make sure that our site is up and running and that your concerns are always addressed. </p>
+	        <!--Code for button if we need:<p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>-->
 	    	</br>
 	    </div>
 	    <div class="col-lg-4">
-	        <h2>Customizable</h2>
-	        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-	        <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+	        <h2 style="text-align:center">Secure</h2>
+          <div style="text-align:center"><img src="images/icons/secure.jpg" alt="..." class="image-bio-thumbnail-borderless-nomargin"></div>
+	        <p>Grant Fellow uses state of the art technology to keep your information safe. Any information you submit is stored on our secure servers, so you can rest assured that no one other than you will have access to your sensitive information.  </p>
 	    	</br>
 	    </div>
 	    <div class="col-lg-4">
-	        <h2>Simple</h2>
-	        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-	        <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
+          <h2 style="text-align:center">Simple</h2>
+          <div style="text-align:center"><img src="images/icons/Simple.jpg" alt="..." class="image-bio-thumbnail-borderless-nomargin"></div>
+	        <p>Grant Fellow is built by students familiar with the grant application process. Our goal is to make finding grants and applying to grants as simple as possible. Our user interface is simple and intuitive, which means you can started in no time.  </p>
 	    	</br>	   
 	    </div>
 	</div>
 
 <hr class="featurette-divider">
-	<div class="row featurette">
-		<div class="col-md-7">
-		  	<h2 class="featurette-heading">Why are we awesome? <span class="text-muted">Because.</span></h2>
-		  	<p class="lead">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-		</div>
-		<div class="col-md-5">
-		  	<img class="featurette-image img-responsive" src="images/professor_temporary.png">
-		</div>
-	</div>
-
-	<hr class="featurette-divider">
-
-</div>
 
 <div id="footer">
     <div class="container">
-    	<p style="float: right;"> Sign Up | Privacy Policy | Contact Us </p>
+    	<p style="float: right;"> <a href="login.php">Sign Up</a> | <a href="privacy.php">Privacy Policy</a> | <a href="contactus.php">Contact Us</a> </p>
     </div>
 </div>
 
@@ -131,5 +147,10 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/jquery.dynatable.js"></script>
 
 </html>
+
+
+</div>
--- js/custom.js
+++ js/custom.js
@@ -28,3 +28,24 @@ $(document).ready(function() {
         $(this).remove();
     })    
 });
+
+$(function () {
+    $( '#table' ).searchable({
+        striped: true,
+        oddRow: { 'background-color': '#f5f5f5' },
+        evenRow: { 'background-color': '#fff' },
+        searchType: 'fuzzy'
+    });
+    
+    $( '#searchable-container' ).searchable({
+        searchField: '#container-search',
+        selector: '.row',
+        childSelector: '.col-xs-4',
+        show: function( elem ) {
+            elem.slideDown(100);
+        },
+        hide: function( elem ) {
+            elem.slideUp( 100 );
+        }
+    })
+});
