49a96d49f93a50a356c85c60939b661a1009e7e1
jljohnlu <jljohnlu@gmail.com>
2014-03-16 16:46:05 -0400

    test
    test

--- designoutput.php
+++ designoutput.php
@@ -8,23 +8,6 @@
  </head>
  <body>
 
- TEST
-
-<?php
-$body = $_POST["body"];
-$size = $_POST["size"];
-$shape = $_POST["shape"];
-
-echo $body;
-
-<br>
-echo $size;
-<br>
-echo $shape;
-echo "error";
-
-?>
-
 <?php
 $body = $_GET['body'];
 echo "GET".$body;
