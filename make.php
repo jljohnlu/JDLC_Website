63c23c74b8023b2176691fdd8a4f6b41d8979271
jljohnlu <jljohnlu@gmail.com>
2014-03-18 19:21:38 -0400

    new changes

--- make.php
+++ make.php
@@ -3,6 +3,8 @@
       <title>Grant Fellow</title>
   </head>
   
+      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
   <link href="css/bootstrap.css" rel="stylesheet">
   <link href="css/custom.css" rel="stylesheet">
   <link href="css/jquery.dynatable.css" rel="stylesheet">
@@ -64,56 +66,45 @@
 </div>
 
 <div class="container-fluid">
-<div class="row">
-    <div class="col-xs-2">
-            <nav class="nav-sidebar">
-                <ul class="nav" style="float:left;">
-            <li><a href="#aggregator">Grant Aggregator</a></li>
-            <li><a href="#commonapp">Common Application</a></li>
-            <li><a href="#appmanager">Application Manager</a></li>
-            </ul>
-            </nav>
-    </div>
-    <div class="col-xs-10">
-    <div class="row">
-    <a name="aggregator"></a> 
-    <h2 style="text-align: center">Grant Aggregator</h2>
-      <div class="col-xs-2">
-          <img src="images/icons/aggregator.jpg" alt="..." class="image-bio-thumbnail-borderless-negativemargin">
-      </div>
-      <div class="col-xs-10">
-          <h4><span class="text-muted">Find all the grants in one place</span></h4>
-          <p class="lead-small">Grant Fellow collects information about both public and private grants and makes them available through our searchable database. Our proprietary matchmaking system also ranks grants based on relevance, so you no longer have to waste time sifting through pages upon pages of irrelevant results.</p>
-      </div>
-    </div>  
-    <hr class="featurette-divider"> 
-
-    <div class="row">
-    <a name="commonapp"></a> 
-    <h2 style="text-align: center">Common Application</h2>
-      <div class="col-xs-2">
-          <img src="images/icons/commonapp.jpg" alt="..." class="image-bio-thumbnail-borderless-negativemargin">
-      </div>
-      <div class="col-xs-10">
-          <h4><span class="text-muted">Save time every time you apply</span></h4>
-          <p class="lead-small">Grant Fellow uses our powerful common application system to save you time. Why type up your personal information or upload your CV every time you apply? Our system stores all that information upon your request on our secure servers, so that you can spend time on other, more important things.</p>
-      </div>
-    </div>  
-    <hr class="featurette-divider"> 
-
-    <div class="row">
-    <a name="appmanager"></a> 
-    <h2 style="text-align: center">Application Manager</h2>
-      <div class="col-xs-2">
-          <img src="images/icons/manager.jpg" alt="..." class="image-bio-thumbnail-borderless-negativemargin">
-      </div>
-      <div class="col-xs-10">
-          <h4><span class="text-muted">Keep track of important files and dates</span></h4>
-          <p class="lead-small">Grant Fellow helps you keep track of all your past applications and upcoming deadlines. We will also send you email reminders to make sure you do not miss anything important. Our beautiful calendar system gives you the option to visualize each application schedule so that you can plan ahead. </p>
-      </div>
-    </div>  
-    <hr class="featurette-divider"> 
-
+	<div class="row">
+	    <div class="col-xs-12">
+	        <ul class="nav nav-pills nav-justified thumbnail setup-panel">
+	            <li class="active"><a href="#step-1">
+	                <h4 class="list-group-item-heading">Step 1</h4>
+	                <p class="list-group-item-text">First step description</p>
+	            </a></li>
+	            <li class="disabled"><a href="#step-2">
+	                <h4 class="list-group-item-heading">Step 2</h4>
+	                <p class="list-group-item-text">Second step description</p>
+	            </a></li>
+	            <li class="disabled"><a href="#step-3">
+	                <h4 class="list-group-item-heading">Step 3</h4>
+	                <p class="list-group-item-text">Third step description</p>
+	            </a></li>
+	        </ul>
+	    </div>
+	</div>
+	<div class="row setup-content" id="step-1">
+	    <div class="col-xs-12">
+	        <div class="col-md-12 well text-center">
+	            <h1> STEP 1</h1>
+	            <button id="activate-step-2" class="btn btn-primary btn-lg">Activate Step 2</button>
+	        </div>
+	    </div>
+	</div>
+	<div class="row setup-content" id="step-2">
+	    <div class="col-xs-12">
+	        <div class="col-md-12 well">
+	            <h1 class="text-center"> STEP 2</h1>
+	        </div>
+	    </div>
+	</div>
+	<div class="row setup-content" id="step-3">
+	    <div class="col-xs-12">
+	        <div class="col-md-12 well">
+	            <h1 class="text-center"> STEP 3</h1>
+	    </div>
+	</div>
   </div>
 </div>
 
@@ -124,8 +115,6 @@
     </div>
 </div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.js"></script>
     <script src="js/custom.js"></script>
