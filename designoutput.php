3d876db01f85f9e48ee348eb7a4a3abf551f79ea
jljohnlu <jljohnlu@gmail.com>
2014-03-29 17:50:35 -0400

    test

--- designoutput.php
+++ designoutput.php
@@ -72,14 +72,18 @@
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-xs-6">
-		<h2>Download</h2>
-		<a href= "<?php echo $body.$size.$shape.'.txt'?>" download>Click here to download the STL code.</a>
-		<a href= "http://www.shapeways.com/create?li=nav">Click here to submit the STL code to shapeways.</a>
+			<div class="alert alert-success">
+				<h2>Download</h2>
+				<a href= "<?php echo $body.$size.$shape.'.txt'?>" download>Click here to download the STL code.</a><br>
+				<a href= "http://www.shapeways.com/create?li=nav">Click here to submit the STL code to shapeways.</a>
+			</div>
 		</div>
 
 		<div class="col-xs-6">
-		<h2>Proceed</h2>
-		<a href= "make.php">Click here to order the button from us</a>
+			<div class="alert alert-info">
+				<h2>Proceed</h2>
+				<a href= "make.php">Click here to order the button from us</a>
+			</div>
 		</div>
 	</div>
 </div>
