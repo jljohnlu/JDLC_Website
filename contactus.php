00779559e9309c23d304b1e6501d26f985f2a30b
jljohnlu <jljohnlu@gmail.com>
2014-04-08 20:29:19 -0400

    final fix for contact us page

--- contactus.php
+++ contactus.php
@@ -121,8 +121,16 @@
             </form>
         </div>
 
-  <hr class="featurette-divider"> 
+<hr class="featurette-divider"> 
     </div>
 </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.js"></script>
+    <script src="js/custom.js"></script>
+    <script src="js/jquery.dynatable.js"></script>
+
 
 </html>
